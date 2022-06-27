deace2c7c55806714517ba37c2048e65f9fc3daf
OllieJC <github@olliejc.uk>
2022-06-27 12:16:42 +0100

    Add txt records

--- dns/route53-vrs-np-sec-gov-uk.tf
+++ dns/route53-vrs-np-sec-gov-uk.tf
@@ -68,11 +68,26 @@ resource "aws_route53_record" "www-aaaa" {
   }
 }
 
+resource "aws_route53_record" "security_txt" {
+  zone_id = aws_route53_zone.vrs-np-sec-gov-uk.zone_id
+  name    = "_security"
+  type    = "TXT"
+
+  records = [
+    "security_policy=https://vulnerability-reporting.service.security.gov.uk/.well-known/security.txt",
+    "security_contact=https://vulnerability-reporting.service.security.gov.uk/submit",
+    "security_contact=mailto:vulnerability-reporting@cabinetoffice.gov.uk"
+  ]
+}
+
 module "vrs-np-aws-r53-parked-domain" {
   source                 = "github.com/co-cddo/aws-route53-parked-govuk-domain//terraform?ref=5e85556ce417cd335c440fd1e7079bd331f443d5"
   zone_id                = aws_route53_zone.vrs-np-sec-gov-uk.zone_id
   depends_on             = [aws_route53_zone.vrs-np-sec-gov-uk]
   email_records          = true  # default
   webserver_records      = false # default
-  additional_txt_records = ["google-site-verification=ZA1BGfGTtQQELKFbcQH85K28Iea-kOF1x0X1O9MZULw"]
+  additional_txt_records = [
+    "google-site-verification=ZA1BGfGTtQQELKFbcQH85K28Iea-kOF1x0X1O9MZULw",
+    "security_policy=https://vulnerability-reporting.service.security.gov.uk/.well-known/security.txt"
+  ]
 }
