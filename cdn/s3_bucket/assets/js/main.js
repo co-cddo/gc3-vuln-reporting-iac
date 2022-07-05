605bc7a011b8895ff993051db9c3ae59dbbcd518
OllieJC <github@olliejc.uk>
2022-07-05 12:11:15 +0100

    Secondary form

--- cdn/s3_bucket/assets/js/main.js
+++ cdn/s3_bucket/assets/js/main.js
@@ -10,3 +10,12 @@ for (var i = 0; i < emails.length; i++) {
   var email = emails[i].innerText.replace("[at]", "@");
   emails[i].innerHTML = '<a href="mailto:'+ email +'">'+ email +"</a>";
 }
+
+function hackerone_onerror() {
+  display_secondary();
+}
+
+function display_secondary() {
+  document.getElementById("submit-default-form").classList.add("hidden");
+  document.getElementById("submit-secondary-form").classList.remove("hidden");
+}
