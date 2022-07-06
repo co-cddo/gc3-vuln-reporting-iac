a0c866a4ad76776478aa4a9567e96cbe0aa95e40
OllieJC <github@olliejc.uk>
2022-07-06 12:42:12 +0100

    Better HackerOne form error handling

--- cdn/s3_bucket/assets/js/main.js
+++ cdn/s3_bucket/assets/js/main.js
@@ -12,10 +12,35 @@ for (var i = 0; i < emails.length; i++) {
 }
 
 function hackerone_onerror() {
-  display_secondary();
+  submit_display_secondary();
 }
 
-function display_secondary() {
+function submit_display_default_form() {
+  document.getElementById("submit-loading").classList.add("hidden");
+  document.getElementById("submit-default-form").classList.remove("hidden");
+  document.getElementById("submit-secondary-form").classList.add("hidden");
+}
+
+function submit_display_secondary() {
+  document.getElementById("submit-loading").classList.add("hidden");
   document.getElementById("submit-default-form").classList.add("hidden");
   document.getElementById("submit-secondary-form").classList.remove("hidden");
+}
+
+function hackerone_check_form() {
+  document.getElementById("submit-loading").classList.remove("hidden");
+  setTimeout(function() {
+    var load_secondary = true;
+    var iframes = document.getElementsByTagName("iframe");
+    if (iframes.length == 1) {
+      if (iframes[0].clientHeight != 1000) {
+        load_secondary = false;
+      }
+    }
+    if (load_secondary) {
+      submit_display_secondary();
+    } else {
+      submit_display_default_form();
+    }
+  }, 2500);
 }
