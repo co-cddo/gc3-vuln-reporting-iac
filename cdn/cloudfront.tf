a780d858e8425dd812e685fadc6752743b9c5feb
Madhuri Nethi <madhuri.nethi1@digital.cabinet-office.gov.uk>
2025-09-23 17:29:11 +0100

    Update TLS version to fix vulnerabilities report errors

--- cdn/cloudfront.tf
+++ cdn/cloudfront.tf
@@ -63,5 +63,6 @@ resource "aws_cloudfront_distribution" "cdn" {
     cloudfront_default_certificate = false
     acm_certificate_arn            = aws_acm_certificate.cdn.arn
     ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
 }
